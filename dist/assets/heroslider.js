03f6a30be2f6986f1b60942f019329d2f6e91a33
BorisMaximenko <125865314+BorisMaximenko@users.noreply.github.com>
2023-03-09 16:23:03 +0300

    test123

---
+++ dist/assets/heroslider.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+  $('.hero__slider').slick({
+    autoplay: true,
+    autoplaySpeed: 2000,
+    prevArrow: '<span class="prev_arrow_main"><i class="fa-solid button-arrow fa-arrow-right"></i></span>',
+    nextArrow: '<span class="next_arrow_main"><i class="fa-solid button-arrow fa-arrow-left"></i></span>'
+  });
+});
