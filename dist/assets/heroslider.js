5853b403121380599b82770cabc317ed021237b4
BorisMaximenko <125865314+BorisMaximenko@users.noreply.github.com>
2023-03-15 15:07:07 +0300

    Delete dist directory

--- dist/assets/heroslider.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function () {
-  $('.hero__slider').slick({
-    autoplay: true,
-    autoplaySpeed: 2000,
-    prevArrow: '<span class="prev_arrow_main"><i class="fa-solid button-arrow fa-arrow-right"></i></span>',
-    nextArrow: '<span class="next_arrow_main"><i class="fa-solid button-arrow fa-arrow-left"></i></span>'
-  });
-});
